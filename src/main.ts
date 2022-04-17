0332f5f081f86baaaccf2a1e7a51c09d69fc1be7
Stas Sribnyi <stas.sribnyi@gmail.com>
2022-04-17 23:54:55 +0300

    Render field with tiles

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,36 @@
-alert('TODO: Match 3')
+const SIZE = 8;
+
+const MOCK_FIELD = [
+  ['🥩', '🎲', '💩', '🎲', '🍭', '🥩', '🍫', '🎲'],
+  ['🍭', '🍫', '💩', '🎲', '🍬', '🍬', '🍭', '🍬'],
+  ['🍫', '💩', '🥩', '🥩', '🎲', '🎲', '🥩', '🍭'],
+  ['🎲', '🍭', '🍫', '🍬', '🍫', '💩', '🍬', '💩'],
+  ['🍬', '🎲', '🍭', '🍫', '🍭', '🥩', '🎲', '🍭'],
+  ['🎲', '🍬', '🎲', '🥩', '🎲', '💩', '🍬', '💩'],
+  ['💩', '🥩', '💩', '🎲', '🍫', '🍭', '🎲', '🍫'],
+  ['🍬', '🍫', '🍬', '🍭', '🥩', '🍫', '🥩', '🍭'],
+].flat();
+
+const field = document.querySelector<HTMLDivElement>('.field');
+
+if (field) {
+  field.style.width = `${SIZE}em`;
+  field.style.height = `${SIZE}em`;
+
+  const tileElements = MOCK_FIELD.map((tile, idx) => {
+    const tileElement = document.createElement('div');
+
+    tileElement.innerText = tile;
+    tileElement.classList.add('tile');
+
+    const row = Math.floor(idx / SIZE);
+    const col = idx - row * SIZE;
+
+    tileElement.style.top = `${row}em`;
+    tileElement.style.left = `${col}em`;
+
+    return tileElement;
+  });
+
+  field?.append(...tileElements);
+}
