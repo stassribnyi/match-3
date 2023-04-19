cc94582729f29c34cbc10265c851217e191d3fb7
Stas Sribnyi <stas.sribnyi@gmail.com>
2023-04-20 01:42:56 +0300

    Remove missing import

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,7 +2,6 @@ export * from './createTile';
 export * from './createBoard';
 export * from './createGrid';
 export * from './createScore';
-export * from './createLevel';
 export * from './createTimer';
 export * from './createMultiplier';
 export * from './createGameOverOverlay';
